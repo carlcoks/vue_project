a36b33bd50228b5f4063281c4be2a7737e81ca76
carlcox <ittruemrdro4@yandex.ru>
2017-08-22 01:44:18 +0500

    404 animation

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,15 +1,45 @@
 <template>
-  <div>
-    <p>
-      404
-    </p>
+  <div class="pageNotFound">
+    <img src="" alt="">
+    <div class="output">
+      <h3>404 - три раза</h3>
+      <div class="Ku">
+        <a @click="showKu()">Ку</a>
+      </div>
+      <transition name="ku-animation">
+        <div class="Ku" v-show="kuShowTwo">
+          <a @click="showKu()">Ку</a>
+        </div>
+      </transition>
+      <transition name="ku-animation">
+        <div class="Ku" v-show="kuShowLast">
+          <router-link tag="a" to="/vuejs">Ку</router-link>
+        </div>
+      </transition>
+    </div>
   </div>
 </template>
 
 <script>
-
+  export default {
+    data () {
+      return {
+        kuShowTwo: false,
+        kuShowLast: false
+      }
+    },
+    methods: {
+      showKu() {
+        if (!this.kuShowTwo) {
+          this.kuShowTwo = true
+        } else {
+          this.kuShowLast = true
+        }
+      }
+    }
+  }
 </script>
 
 <style lang="scss" scoped>
-
+  @import '../assets/style/404';
 </style>
