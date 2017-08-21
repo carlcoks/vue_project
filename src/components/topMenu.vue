e1e5b57630334a1600f201341d435d3c7f194131
carlcox <ittruemrdro4@yandex.ru>
2017-08-22 00:50:34 +0500

    Menu - routing ready

---
+++ src/components/topMenu.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="topMenu">
+    <div class="logo">
+      <img src="" alt="">
+    </div>
+    <div class="menu">
+      <router-link tag="a" to="/vuejs">VueJS</router-link>
+      <router-link tag="a" to="/cpp">C++</router-link>
+      <router-link tag="a" to="/js">JS</router-link>
+      <router-link tag="a" to="/linux">Linux</router-link>
+    </div>
+    <div class="auth">
+      <router-link tag="a" to="/auth">Вход</router-link>
+    </div>
+  </div>
+</template>
+
+<script>
+
+</script>
+
+<style lang="scss" scoped>
+  @import '../assets/style/topMenu';
+</style>
